8f69a30fcbee4d5a4f219465b1cad3b1ebc3a8d1
kayodexx1 <ekundayokayodeonimisi@gmail.com>
2023-08-10 08:12:43 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+include < stdio.h >
 /**
  * main - Program printing stuff
  * Return: 0 (Success)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
-include < stdio.h >
+#include < stdio.h >
 /**
- * main - Program printing line
- * Return: 0 (Success)
+ * main -Program printing line
+ * Return:0 (Success)
  */
-
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n")
+{printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }	
