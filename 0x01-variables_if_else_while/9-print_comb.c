064bd6fc15866752f093a60d67634372050f0a20
kayodexx1 <ekundayokayodeonimisi@gmail.com>
2023-08-11 19:33:58 -0700

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - prints all possible combinations of single-digit numbers
- * return: Always 0 (Success)
+ * main - Frog Entry point
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n;
+	int a;
 
-	for (n = 48; n = 58; n++)
+	for (a = 0; a < 10; a++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(a + '0');
+		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
